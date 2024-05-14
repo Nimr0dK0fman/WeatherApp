5789393d16e28fdb970cb21abe21816e3717f8c9
Nimr0dK0fman <nimrodjkofman@gmail.com>
2024-05-14 17:03:59 -0400

    added details to the crypto data and aligned elements

--- index.js
+++ index.js
@@ -9,17 +9,20 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
     })
 
-    //crypto price alert
+//crypto price alert
 
-    fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     .then(res => {
         if (!res.ok) {
             throw Error("Something went wrong")
         }
-        console.log(res.status)
         return res.json()
     })
     .then(data => {
-        console.log(data)
+        document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `
     })
     .catch(err => console.error(err))
