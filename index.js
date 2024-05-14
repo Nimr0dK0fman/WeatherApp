68ad1d370c1661f5a083e1a8731a4cdf09092129
Nimr0dK0fman <nimrodjkofman@gmail.com>
2024-05-14 17:44:00 -0400

    added manifest file and a logo for the dashboard app

--- index.js
+++ index.js
@@ -38,5 +38,26 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
         document.getElementById("time").textContent = date.toLocaleDateString("en-us", {timeStyle:"medium"})
     
     }
-
     setInterval(getCurrentTime,1000)
+
+
+    //weather set up
+
+    navigator.geolocation.getCurrentPosition(position => {
+        fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+            .then(res => {
+                if (!res.ok) {
+                    throw Error("Weather data not available")
+                }
+                return res.json()
+            })
+            .then(data => {
+                const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+                document.getElementById("weather").innerHTML = `
+                    <img src=${iconUrl} />
+                    <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                    <p class="weather-city">${data.name}</p>
+                `
+            })
+            .catch(err => console.error(err))
+    });
